d9598e2c5f91fc61f3c0b26eae91c031346c6fda
Yassir aittata <yassir.aittata@gmail.com>
2024-02-03 22:15:00 +0000

    create user

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto, UpdateUserDto, User } from '@app/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class UsersService {
+export class UsersService implements OnModuleInit {
+  private readonly users: User[];
+
+  onModuleInit() {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const user: User = {
+      id: randomUUID(),
+      ...createUserDto,
+      subscribed: false,
+      socilaMedia: {},
+    };
+
+    this.users.push(user);
+
+    return user;
   }
 
   findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  update(id: string, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} user`;
   }
 }
